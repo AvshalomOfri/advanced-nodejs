61ec9791863f337c8cb11511d3a84945e94792fc
AvshalomOfri <swmmws20@gmail.com>
2021-04-29 19:55:29 +0300

    2nd

--- 1.5/StringDecoder.js
+++ 1.5/StringDecoder.js
@@ -1,12 +1,13 @@
-const { StringDecoder } = require('string_decoder');
-const decoder = new StringDecoder('utf8');
+//when converting streams of binary data we should use the StringDecoder module because it handles multibyte characters much better, especially incomplete mutibyte characters. the string decoder preserves the incomplete encoded characters internally until its complete and then returns the result, the default toString() operation on the buffer does not do that
+const { StringDecoder } = require("string_decoder").StringDecoder;
+const decoder = new StringDecoder("utf8");
 
-process.stdin.on('readable', () => {
+process.stdin.on("readable", () => {
   const chunk = process.stdin.read();
   if (chunk != null) {
     const buffer = Buffer.from([chunk]);
-    console.log('With .toString():', buffer.toString());
-    console.log('With StringDecoder:', decoder.write(buffer));
+    console.log("With .toString():", buffer.toString());
+    console.log("With StringDecoder:", decoder.write(buffer));
   }
 });
 
--- 1.5/buff.slice.js
+++ 1.5/buff.slice.js
@@ -1,19 +1,19 @@
-const fs = require('fs');
+const fs = require("fs");
 
 const conversionMap = {
-  '88': '65',
-  '89': '66',
-  '90': '67',
+  88: "71",
+  89: "72",
+  90: "73",
 };
 
 fs.readFile(__filename, (err, buffer) => {
-  let tag = buffer.slice(-4, -1);
+  let tag = buffer.slice(-5, -1);
 
-  for(let i=0;i < tag.length; i++) {
+  for (let i = 0; i < tag.length; i++) {
     tag[i] = conversionMap[tag[i]];
   }
 
   console.log(buffer.toString());
 });
-
+// Even though a "tag" variable has been assigned a slice of the buffer and its charaters has been converted, the original buffer has been modified as well because the sliced buffer and the original one share the same memory space
 // TAG: XYZ
