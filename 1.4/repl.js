3d1275205e31a85669e4c752c10425ffd9668e07
AvshalomOfri <swmmws20@gmail.com>
2021-04-29 19:07:42 +0300

    1st

--- 1.4/repl.js
+++ 1.4/repl.js
@@ -1,8 +1,9 @@
-const repl = require('repl');
+//you can customize your REPL session
 
+const repl = require("repl");
 let r = repl.start({
   ignoreUndefined: true,
-  replMode: repl.REPL_MODE_STRICT
+  replMode: repl.REPL_MODE_STRICT,
 });
 
-r.context.lodash = require('lodash');
+r.context.lodash = require("lodash");
