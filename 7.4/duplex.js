5f2ac6d88cbbc455fa39f18666058fec5768c6c5
AvshalomOfri <swmmws20@gmail.com>
2021-09-13 17:31:37 +0200

    13.9.2021 laptop

--- 7.4/duplex.js
+++ 7.4/duplex.js
@@ -1,4 +1,5 @@
-const { Duplex } = require('stream');
+//every duplex stream must have both write and read methods. It important to understand that the read and write components are operating independently from one another, this is merely a grouping of two featues in one object
+const { Duplex } = require("stream");
 
 const inoutStream = new Duplex({
   write(chunk, encoding, callback) {
@@ -9,10 +10,11 @@ const inoutStream = new Duplex({
   read(size) {
     if (this.currentCharCode > 90) {
       this.push(null);
+      console.log(size);
       return;
     }
     this.push(String.fromCharCode(this.currentCharCode++));
-  }
+  },
 });
 
 inoutStream.currentCharCode = 65;
